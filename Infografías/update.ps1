9a5102c0653b7f691e64e61f7c1a6ee48d7db913
ProfEdgarH <edhemo@gmail.com>
2026-05-16 21:28:00 -0500

    Delete Infografías directory

--- Infografías/update.ps1
+++
@@ -1,5 +0,0 @@
-$json = Get-Content -Raw -Encoding UTF8 'JSON.txt'
-$script = Get-Content -Raw -Encoding UTF8 'script.js'
-$script = $script -replace '(?s)const DEFAULT_TEMPLATES = \{.*?\n\};\n', ("const DEFAULT_TEMPLATES = " + $json + ";`n")
-$script = $script.Replace('.slice(0,7)', '.slice(0,38)')
-Set-Content -Path 'script.js' -Value $script -Encoding UTF8
